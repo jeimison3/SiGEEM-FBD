066582888c327346715db537ace51dc0c67e4279
moiseslovegod-png <moiseslovegod@gmail.com>
2026-01-14 20:34:22 -0300

    Add Turma and Avaliacao models to database

--- database/models.py
+++ database/models.py
@@ -2,6 +2,18 @@ from sqlalchemy import Column, Integer, String, Float, Date, ForeignKey
 from sqlalchemy.orm import relationship
 from database.connection import Base
 
+class Turma(Base):
+    __tablename__ = 'turma'
+    id_turma = Column(Integer, primary_key=True)
+    nome = Column(String(50), nullable=False)
+    sala = Column(String(10))
+
+class Avaliacao(Base):
+    __tablename__ = 'avaliacoes'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    turma_id = Column(Integer, ForeignKey('turma.id_turma'), nullable=False)
+
 class Usuario(Base):
     __tablename__ = 'usuarios'
     
@@ -10,13 +22,6 @@ class Usuario(Base):
     password = Column(String(255), nullable=False)
     nome = Column(String(100))
 
-class Turma(Base):
-    __tablename__ = 'turmas'
-    
-    id = Column(Integer, primary_key=True)
-    nome = Column(String(100), nullable=False)
-    ano = Column(Integer)
-    turno = Column(String(20))
 
 class Aluno(Base):
     __tablename__ = 'alunos'
